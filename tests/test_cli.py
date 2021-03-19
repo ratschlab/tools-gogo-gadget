25ec4ce4f6cb17b1297027fa36c045d372593193
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2021-03-19 14:48:44 +0100

    Feature/zsh support (#2)
    * switching to click 7
    
    * remove unessecary invoke dependency
    
    * remove obsolete collect_ignore config

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,9 +13,9 @@ def runner():
 
 
 def test_cli_base(runner):
-    cmd_names = ['foo', 'bar', 'more_foo', 'less_bar']
+    cmd_names = ['foo', 'bar', 'more-foo', 'less-bar']
 
-    cmd_dict = {c: {'cmd': 'some_command', 'help': 'blabla'} for c in
+    cmd_dict = {c: {'cmd': 'some-command', 'help': 'blabla'} for c in
                 cmd_names}
 
     cli.build_command(cmd_dict)
@@ -27,7 +27,7 @@ def test_cli_base(runner):
 
 
 def test_cli_base_execution(runner):
-    cmd_dict = {'test_command': {'cmd': "touch", 'help': 'just calls touch'}}
+    cmd_dict = {'test-command': {'cmd': "touch", 'help': 'just calls touch'}}
 
     cli.build_command(cmd_dict)
 
@@ -35,34 +35,34 @@ def test_cli_base_execution(runner):
         p = Path(t, 'testfile')
 
         assert not p.exists()
-        runner.invoke(cli.main_cli, args=['test_command', str(p)])
+        runner.invoke(cli.main_cli, args=['test-command', str(p)])
         assert p.exists()
 
 
 def test_cli_base_not_parse_option(runner):
-    cmd_dict = {'test_command': {'cmd': "echo", 'help': ''}}
+    cmd_dict = {'test-command': {'cmd': "echo", 'help': ''}}
 
     cli.build_command(cmd_dict)
 
-    result = runner.invoke(cli.main_cli, args=['test_command', 'balbla', '-p',
+    result = runner.invoke(cli.main_cli, args=['test-command', 'balbla', '-p',
                                                '-x'])
     assert result.exit_code == 0
 
 
 def test_cli_nested(runner):
-    cmd_dict = {'test_command': {'cmd': "hello", 'help': ''},
+    cmd_dict = {'test-command': {'cmd': "hello", 'help': ''},
                 'group1':
                     {'nested1': {'cmd': 'nested1', 'help': ''}},
                 'group2':
-                    {'nested2_1':
+                    {'nested2-1':
                         {
-                            'nested2_1_1': {'cmd': 'nested2_1_1'},
-                            'nested2_1_2': {'cmd': 'nested2_1_2'}
+                            'nested2-1-1': {'cmd': 'nested2-1-1'},
+                            'nested2-1-2': {'cmd': 'nested2-1-2'}
                         },
-                        'nested2_2':
+                        'nested2-2':
                             {
-                                'nested2_2_1': {'cmd': 'nested2_2_1'},
-                                'nested2_1_2': {'cmd': 'nested2_1_2'}
+                                'nested2-2-1': {'cmd': 'nested2-2-1'},
+                                'nested2-1-2': {'cmd': 'nested2-1-2'}
                             }
                     }
                 }
@@ -70,18 +70,18 @@ def test_cli_nested(runner):
     cli.build_command(cmd_dict)
 
     result = runner.invoke(cli.main_cli)
-    assert all(x in result.output for x in ['test_command', 'group1', 'group2'])
+    assert all(x in result.output for x in ['test-command', 'group1', 'group2'])
 
     result = runner.invoke(cli.main_cli, args=['group1'])
     assert all(x in result.output for x in ['nested1'])
 
     result = runner.invoke(cli.main_cli, args=['group2'])
-    assert all(x in result.output for x in ['nested2_1', 'nested2_2'])
+    assert all(x in result.output for x in ['nested2-1', 'nested2-2'])
 
 
 def test_is_flat_dict():
-    assert cli._is_flat_dict({}) == True
-    assert cli._is_flat_dict({'hello': 'world', 1: Path('bla')}) == True
+    assert cli._is_flat_dict({})
+    assert cli._is_flat_dict({'hello': 'world', 1: Path('bla')})
 
     assert cli._is_flat_dict({'hello': 'world', "x": {}}) == False
     assert cli._is_flat_dict({'hello': 'world', "x": {'foo': 'bar'}}) == False
